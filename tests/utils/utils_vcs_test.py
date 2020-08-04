534025887ee0151d10abacba6e82eb0e9efbd97d
Frithjof Gressmann <frithjof@graphcore.ai>
2020-08-04 19:36:57 +0100

    Relax get_root_commit test assumption to make test pass when using shallow clone

--- tests/utils/utils_vcs_test.py
+++ tests/utils/utils_vcs_test.py
@@ -2,4 +2,4 @@ from machinable.utils.vcs import get_root_commit
 
 
 def test_get_root_commit():
-    assert get_root_commit(".") == "0f831188de08028e36d59e9acd79700f70f114ed"
+    assert len(get_root_commit(".")) == 40
