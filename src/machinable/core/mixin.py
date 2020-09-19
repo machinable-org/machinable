d98312c65fa134409941c47480715613f7148939
frthjf <lifeworld-api@posteo.net>
2020-09-19 21:14:08 +0100

    Rename mixin config to hidden attribute

--- src/machinable/core/mixin.py
+++ src/machinable/core/mixin.py
@@ -4,7 +4,7 @@ from ..config.parser import ModuleClass
 
 class MixinInstance:
     def __init__(self, controller, mixin_class, attribute):
-        self.config = {
+        self._binding = {
             "controller": controller,
             "class": mixin_class,
             "attribute": attribute,
@@ -12,33 +12,33 @@ class MixinInstance:
 
     def __getattr__(self, item):
         # lazy-load class
-        if isinstance(self.config["class"], ModuleClass):
-            self.config["class"] = self.config["class"].load(instantiate=False)
+        if isinstance(self._binding["class"], ModuleClass):
+            self._binding["class"] = self._binding["class"].load(instantiate=False)
 
-        attribute = getattr(self.config["class"], item, None)
+        attribute = getattr(self._binding["class"], item, None)
 
         if attribute is None:
             raise AttributeError(
-                f"Mixin '{self.config['class'].__name__}' has no method '{item}'"
+                f"Mixin '{self._binding['class'].__name__}' has no method '{item}'"
             )
 
         if isinstance(attribute, property):
-            return attribute.fget(self.config["controller"])
+            return attribute.fget(self._binding["controller"])
 
         if not callable(attribute):
             return attribute
 
-        if isinstance(getattr_static(self.config["class"], item), staticmethod):
+        if isinstance(getattr_static(self._binding["class"], item), staticmethod):
             return attribute
 
         # if attribute is non-static method we decorate it to pass in the controller
 
         def bound_method(*args, **kwargs):
             # bind mixin instance to controller for mixin self reference
-            self.config["controller"].__mixin__ = getattr(
-                self.config["controller"], self.config["attribute"]
+            self._binding["controller"].__mixin__ = getattr(
+                self._binding["controller"], self._binding["attribute"]
             )
-            output = attribute(self.config["controller"], *args, **kwargs)
+            output = attribute(self._binding["controller"], *args, **kwargs)
 
             return output
 
