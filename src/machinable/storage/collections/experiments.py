d80fde288d0beb4062b64d1dc8a55371b7064b1b
frthjf <lifeworld-api@posteo.net>
2020-09-20 17:02:45 +0100

    Add components shortcut on ExperimentCollection

--- src/machinable/storage/collections/experiments.py
+++ src/machinable/storage/collections/experiments.py
@@ -1,5 +1,15 @@
 from .base import Collection
+from .components import ComponentStorageCollection
 
 
 class ExperimentStorageCollection(Collection):
-    pass
+    @property
+    def components(self):
+        if len(self._items) == 0:
+            return ComponentStorageCollection()
+        components = self._items[0].components
+        if len(self._items) == 1:
+            return components
+        for experiment in self._items[1:]:
+            components.merge(experiment.components)
+        return ComponentStorageCollection(components.unique(lambda x: x.component_id))
