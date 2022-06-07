45f815351dac13536178736007b3e158ff4536b6
frthjf <frithjof@noc.io>
2022-06-07 11:17:15 -0500

    Increase test coverage

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,11 +1,12 @@
 import os
 
-import pytest
-from machinable import Project, errors
+from machinable import Project
 
 
 def test_project():
     project = Project()
     assert project.__model__.directory == os.getcwd()
-    project = Project("tests/samples/project")
+    project = Project("tests/samples/project", name="test")
+    assert project.name() == "test"
+    assert project.path().endswith("samples/project")
     project.connect()
