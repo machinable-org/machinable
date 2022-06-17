676b48cf75d2e1fa060842ac934ea853f9b61590
frthjf <lifeworld-api@posteo.net>
2022-06-17 03:46:38 +0000

    Deploying to gh-pages from @ machinable-org/machinable@9be9db1c38e774e1b31d13a983fc7ea70de2c34b 🚀

---
+++ assets/index.html.8adf8ddc.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"",lang:"en-US",frontmatter:{home:!0,heroImage:"/hero.png",footer:"MIT Licensed"},excerpt:"",headers:[],git:{updatedTime:165543755e4,contributors:[{name:"dependabot[bot]",email:"49699333+dependabot[bot]@users.noreply.github.com",commits:1}]},filePathRelative:"README.md"};export{e as data};
