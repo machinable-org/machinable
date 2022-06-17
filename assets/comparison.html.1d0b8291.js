f30c118af22c901856f1fee2b1bf9f76e33ca728
frthjf <lifeworld-api@posteo.net>
2022-06-17 03:47:21 +0000

    Deploying to gh-pages from @ machinable-org/machinable@9c958f8501bb862e5d046072496a11d793bcf5ff 🚀

--- assets/comparison.html.1d0b8291.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-23441c14",path:"/miscellaneous/comparison.html",title:"Comparison with alternatives",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:165543755e4,contributors:[{name:"dependabot[bot]",email:"49699333+dependabot[bot]@users.noreply.github.com",commits:1}]},filePathRelative:"miscellaneous/comparison.md"};export{e as data};
