1d855a34554e97e389e622f342f4b8d37b18b44c
frthjf <lifeworld-api@posteo.net>
2022-07-29 16:34:39 +0000

    Deploying to gh-pages from @ machinable-org/machinable@9add9641a3b6c1a777c05f0c73435502b6c41624 🚀

---
+++ assets/design-principles.html.c491117f.js
@@ -0,0 +1,1 @@
+const e={key:"v-d680cf26",path:"/miscellaneous/design-principles.html",title:"Design Principles",lang:"en-US",frontmatter:{sidebar:"auto"},excerpt:"",headers:[],git:{updatedTime:1659112379e3,contributors:[{name:"dependabot[bot]",email:"49699333+dependabot[bot]@users.noreply.github.com",commits:1}]},filePathRelative:"miscellaneous/design-principles.md"};export{e as data};
