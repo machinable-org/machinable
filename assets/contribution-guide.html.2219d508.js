62a4e96eaf7b44311dad0e8e5ff1de43d2664896
frthjf <lifeworld-api@posteo.net>
2022-07-29 16:31:09 +0000

    Deploying to gh-pages from @ machinable-org/machinable@4537aa6de888a747975a707244843c00bd0eff0e 🚀

--- assets/contribution-guide.html.2219d508.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-5b6fbac0",path:"/miscellaneous/contribution-guide.html",title:"Contribution guide",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1658802417e3,contributors:[{name:"dependabot[bot]",email:"49699333+dependabot[bot]@users.noreply.github.com",commits:1}]},filePathRelative:"miscellaneous/contribution-guide.md"};export{e as data};
