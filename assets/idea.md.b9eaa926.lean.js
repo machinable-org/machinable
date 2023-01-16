d6a89fe3d5c8dbb2ba46b7d550663361b893cd25
frthjf <lifeworld-api@posteo.net>
2023-01-16 19:37:53 +0000

    Deploying to gh-pages from @ machinable-org/machinable@a316eb651ba801f58c500719812be6b41d8e2c15 🚀

---
+++ assets/idea.md.b9eaa926.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,d as l}from"./app.c71878a1.js";const C=JSON.parse('{"title":"Rationale and design considerations","description":"","frontmatter":{},"headers":[],"relativePath":"idea.md"}'),p={name:"idea.md"},o=l("",15),e=[o];function t(c,r,F,y,i,D){return a(),n("div",null,e)}const u=s(p,[["render",t]]);export{C as __pageData,u as default};
